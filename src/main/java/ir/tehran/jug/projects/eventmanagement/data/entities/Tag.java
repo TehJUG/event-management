9444d0ef87f618d55deb04148f4fb8c98c530ae9
JavaBaz <134164700+JavaBaz@users.noreply.github.com>
2024-01-10 16:10:36 +0330

    feat: add Tag entity.

---
+++ src/main/java/ir/tehran/jug/projects/eventmanagement/data/entities/Tag.java
@@ -0,0 +1,31 @@
+package ir.tehran.jug.projects.eventmanagement.data.entities;
+
+import ir.tehran.jug.projects.eventmanagement.data.base.entity.BaseEntity;
+import ir.tehran.jug.projects.eventmanagement.data.entities.events.Event;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+import java.util.Set;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@SuperBuilder
+@Entity
+@Table(name = "TAGS")
+public class Tag extends BaseEntity<Long> {
+
+    @Column(unique = true)
+    @NotBlank(message = "title can't be blank")
+    private String title;
+
+
+    @ManyToMany(mappedBy = "tags")
+    private Set<Event> events;
+}
