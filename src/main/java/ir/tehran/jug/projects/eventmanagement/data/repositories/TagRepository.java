537e93f5f18966102a8bafc884fade77d0875780
JavaBaz <134164700+JavaBaz@users.noreply.github.com>
2024-01-10 16:11:46 +0330

    feat: add Tag repository interface.

---
+++ src/main/java/ir/tehran/jug/projects/eventmanagement/data/repositories/TagRepository.java
@@ -0,0 +1,8 @@
+package ir.tehran.jug.projects.eventmanagement.data.repositories;
+
+import ir.tehran.jug.projects.eventmanagement.data.base.repository.BaseRepository;
+import ir.tehran.jug.projects.eventmanagement.data.entities.Tag;
+
+public interface TagRepository extends BaseRepository<Tag, Long> {
+    Tag findByTitle(String tagTitle);
+}
