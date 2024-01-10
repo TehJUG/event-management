05a2c7d968fdc27bfe2bc08514cfa63729775d63
JavaBaz <134164700+JavaBaz@users.noreply.github.com>
2024-01-10 16:12:45 +0330

    feat: add offlineEvent repository interface.

---
+++ src/main/java/ir/tehran/jug/projects/eventmanagement/data/service/OfflineEventService.java
@@ -0,0 +1,7 @@
+package ir.tehran.jug.projects.eventmanagement.data.service;
+
+import ir.tehran.jug.projects.eventmanagement.data.base.service.BaseService;
+import ir.tehran.jug.projects.eventmanagement.data.entities.events.OfflineEvent;
+
+public interface OfflineEventService extends BaseService<OfflineEvent,Long> {
+}
