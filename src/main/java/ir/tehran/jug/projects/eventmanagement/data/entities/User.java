6265c6778074354eb07264f021a4049aec12c5dd
hryesf <houriye.esfahanian@gmail.com>
2024-02-02 23:40:45 +0330

    refactor : remove unused import
    Trello Link : https://trello.com/c/gAOvwAJY

--- src/main/java/ir/tehran/jug/projects/eventmanagement/data/entities/User.java
+++ src/main/java/ir/tehran/jug/projects/eventmanagement/data/entities/User.java
@@ -1,6 +1,5 @@
 package ir.tehran.jug.projects.eventmanagement.data.entities;
 
-import ir.tehran.jug.projects.eventmanagement.data.base.entity.BaseEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.OneToMany;
