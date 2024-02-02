2c5acd09d260599282bf64fed687f6884a2c20bc
JavaBaz <Bazgir.civil@gmail.com>
2024-02-02 21:04:10 +0330

    Refactor: remove unnecessary imports.

--- src/main/java/ir/tehran/jug/projects/eventmanagement/data/repositories/CategoryRepository.java
+++ src/main/java/ir/tehran/jug/projects/eventmanagement/data/repositories/CategoryRepository.java
@@ -2,7 +2,6 @@ package ir.tehran.jug.projects.eventmanagement.data.repositories;
 
 import ir.tehran.jug.projects.eventmanagement.data.entities.Category;
 
-import java.util.Optional;
 
 public interface CategoryRepository extends BaseRepository<Category, Long> {
 }
--- src/main/java/ir/tehran/jug/projects/eventmanagement/data/repositories/OfflineEventRepository.java
+++ src/main/java/ir/tehran/jug/projects/eventmanagement/data/repositories/OfflineEventRepository.java
@@ -2,7 +2,6 @@ package ir.tehran.jug.projects.eventmanagement.data.repositories;
 
 import ir.tehran.jug.projects.eventmanagement.data.entities.events.OfflineEvent;
 
-import java.util.Optional;
 
 public interface OfflineEventRepository extends BaseRepository<OfflineEvent, Long> {
 
