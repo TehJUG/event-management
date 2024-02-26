5edb0f87a448d990043558f6de1c32c843eed3cf
JavaBaz <134164700+JavaBaz@users.noreply.github.com>
2024-02-26 12:38:09 +0330

    Refactor: remove unused imports.

--- src/main/java/ir/tehran/jug/projects/eventmanagement/data/repositories/OrganizationRepository.java
+++ src/main/java/ir/tehran/jug/projects/eventmanagement/data/repositories/OrganizationRepository.java
@@ -1,6 +1,5 @@
 package ir.tehran.jug.projects.eventmanagement.data.repositories;
 
-import ir.tehran.jug.projects.eventmanagement.data.base.repository.BaseRepository;
 import ir.tehran.jug.projects.eventmanagement.data.entities.Organization;
 
 public interface OrganizationRepository extends BaseRepository<Organization, Long> {
