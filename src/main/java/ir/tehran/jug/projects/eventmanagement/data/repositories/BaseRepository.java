287e1a925fc11572007e5c61df2fc1ea6313ad28
JavaBaz <134164700+JavaBaz@users.noreply.github.com>
2024-02-26 14:32:27 +0330

    Refactor: remove method from BaseRepository.

--- src/main/java/ir/tehran/jug/projects/eventmanagement/data/repositories/BaseRepository.java
+++ src/main/java/ir/tehran/jug/projects/eventmanagement/data/repositories/BaseRepository.java
@@ -4,9 +4,7 @@ import ir.tehran.jug.projects.eventmanagement.data.entities.BaseEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.io.Serializable;
-import java.util.Optional;
 
 public interface BaseRepository<E extends BaseEntity<ID>, ID extends Serializable> extends JpaRepository<E, ID> {
-    Optional<E> findByTitle(String title);
 
 }
