c789eca47449f9173a476c83db1403f98d128d89
JavaBaz <134164700+JavaBaz@users.noreply.github.com>
2024-02-26 14:33:08 +0330

    Feat: add "findByTitle" method to TagRepository.

--- src/main/java/ir/tehran/jug/projects/eventmanagement/data/repositories/TagRepository.java
+++ src/main/java/ir/tehran/jug/projects/eventmanagement/data/repositories/TagRepository.java
@@ -2,5 +2,9 @@ package ir.tehran.jug.projects.eventmanagement.data.repositories;
 
 import ir.tehran.jug.projects.eventmanagement.data.entities.Tag;
 
+import java.util.Optional;
+
 public interface TagRepository extends BaseRepository<Tag, Long> {
+    Optional<Tag> findByTitle(String title);
+
 }
